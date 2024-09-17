58a32c82eb9042cd2ccd4d882dc951da5afefb0c
Sean Marcus Jensen <itskolesean@gmail.com>
2024-09-17 15:52:01 +0200

    Massive Changes

--- problem_2/utils/finance/finance_manager.py
+++ problem_2/utils/finance/finance_manager.py
@@ -8,121 +8,53 @@ Returns:
 _type_: _description_
 """
 
-import yfinance as fy
-import datetime
 import pandas as pd
-from currency_converter import CurrencyConverter
 
-class Company:
-    """_summary_
+class StockCalculator:
 
-    Returns:
-        Company: A Company object.
-    """
-    currency = 'USD'
-    __valuta_converter = CurrencyConverter() # Private
+    @staticmethod
+    def monthly_returns(df: pd.DataFrame) -> pd.DataFrame:
+        """ Calculate the monthly returns for the given DataFrame.
+        Args:
+            data_frame (pd.DataFrame): A DataFrame with the data.
+        Returns:
+            pd.DataFrame: A DataFrame with the monthly returns.
+        """
+        # Ensure the DateTime index is in UTC
+        df.index = pd.to_datetime(df.index, utc=True)
 
-    def __init__(self, ticker: fy.Ticker, history_func) -> None:
-        self.__ticker = ticker
-        self.data_frame: pd.DataFrame = history_func(ticker)
-        self.__convert_currency()
+        # Initialize an empty DataFrame to store the results
+        all_monthly_returns = pd.DataFrame()
 
-        self.__monthly_returns: pd.DataFrame = pd.DataFrame()
-    
-    def as_dataframe(self) -> pd.DataFrame:
-        """ Get the data as a DataFrame. """
-        return self.data_frame
-    
-    def get_monthly_returns(self) -> pd.DataFrame:
-        if self.__monthly_returns is None or self.__monthly_returns.empty:
-            monthly_df = self.data_frame.reset_index()
-            monthly_df['Date'] = pd.to_datetime(monthly_df['Date'])
-            monthly_df.set_index('Date', inplace=True)
+        # Group by ticker
+        tickers_group = df.groupby('Ticker')
 
-            # Group by year and month
-            monthly_group = monthly_df.groupby([monthly_df.index.year, monthly_df.index.month])
+        for ticker, df in tickers_group:
+            # Group by year and month within each ticker group
+            monthly_group = df.groupby([df.index.year, df.index.month])
 
             # Calculate the first and last values for each month
             first_date = monthly_group.first()
             last_date = monthly_group.last()
 
-
             # Calculate monthly returns using the 'Close' values
-            monthly_returns = last_date['Close'].pct_change().dropna().rename('Monthly Returns(%)')
+            monthly_returns = last_date['Close'].pct_change().dropna().rename('Returns')
             monthly_returns = monthly_returns.to_frame().reset_index(drop=True)
 
             # Assign the 'Open' and 'Close' values for each month
             monthly_returns['Open'] = first_date['Open'].values[1:]  # Skip the first value to match pct_change
             monthly_returns['Close'] = last_date['Close'].values[1:]  # Skip the first value to match pct_change
 
-            monthly_returns['Date'] = last_date.index[1:]
+            # Add the 'Date' column
+            monthly_returns['Date'] = [pd.Timestamp(year=year, month=month, day=1) for year, month in last_date.index[1:]]
 
             # Add the 'Ticker' column
-            monthly_returns['Ticker'] = self.__ticker.ticker
+            monthly_returns['Ticker'] = ticker
 
-            monthly_returns = monthly_returns[['Date', 'Ticker', 'Open', 'Close', 'Monthly Returns(%)']]
+            # Reorder columns to have 'Date', 'Ticker', 'Open', 'Close', 'Returns'
+            monthly_returns = monthly_returns[['Date', 'Ticker', 'Open', 'Close', 'Returns']]
 
+            # Append the results to the all_monthly_returns DataFrame
+            all_monthly_returns = pd.concat([all_monthly_returns, monthly_returns], ignore_index=True)
 
-            self.__monthly_returns = monthly_returns
-
-        return self.__monthly_returns
-
-    def __convert_currency(self) -> None:
-        __valuta_columns = ['Open', 'High', 'Low', 'Close']
-        ticker_currency = self.__ticker.info['currency'] if 'currency' in self.__ticker.info else self.currency
-        _currency_conversion = lambda x: self.__valuta_converter.convert(x, ticker_currency, self.currency)
-        self.data_frame[__valuta_columns] = self.data_frame[__valuta_columns].map(_currency_conversion)
-
-    def __getitem__(self, key: str) -> pd.DataFrame:
-        """ Get the data for a specific ticker. """
-        return self.data_frame[key]
-
-class TickerManager:
-
-    __accepted_intervals = ['1m', '2m', '5m', '15m', '30m',
-                            '1h', '1d', '5d',
-                            '1wk', '1mo', '3mo']
-
-    def __init__(self, tickers: list[str],
-                 start: datetime.datetime,
-                 end: datetime.datetime,
-                 interval: str = '1d') -> None:
-        """ Create a TickerManager object.
-
-        Args:
-            tickers (list[str]): A list of tickers to be used.
-            start (datetime.datetime): Start date to get the data from.
-            end (datetime.datetime): End date to get the data from.
-            interval (str, optional): _description_. Defaults to '1d'.
-
-        Raises:
-            ValueError: The interval is not accepted.
-        """
-
-        if interval not in self.__accepted_intervals:
-            raise ValueError(f'Interval \'{interval}\' not accepted. Accepted intervals are: {self.__accepted_intervals}')
-
-        loaded = fy.Tickers(tickers)
-        self.companies: dict[str, Company] = {}
-        for k in loaded.tickers.keys():
-            self.companies[k] = Company(loaded.tickers[k], lambda x: x.history(period=interval, start=start, end=end))
-
-    def get_stock_history(self) -> pd.DataFrame:
-        """ Get the stock history for a specific ticker. """
-        data = pd.DataFrame()
-        for k, v in self.companies.items():
-            data_frame = v.as_dataframe()
-            data_frame['Ticker'] = k
-            data = pd.concat([data, data_frame], axis=0)
-        return data
-    
-    def get_ticker_count(self) -> int:
-        """ Get the number of tickers. """
-        return len(self.companies.keys())
-
-    def get(self, key: str) -> Company:
-        return self.companies[key]
-
-    def __getitem__(self, key: str) -> pd.DataFrame:
-        """ Get the data for a specific ticker. """
-        return self.companies[key].as_dataframe()
+        return all_monthly_returns
---
+++ problem_2/utils/plots/stock_history.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class StockPlotter:
+
+    @staticmethod
+    def stock_history(dataframe: pd.DataFrame, title: str) -> None:
+        # Ensure 'Date' column is in datetime format
+        df = dataframe.reset_index()
+        df['Date'] = pd.to_datetime(df['Date'], utc=True)
+        df.set_index('Date', inplace=True)
+
+        # Group by 'Ticker'
+        tickers = df.groupby('Ticker')
+
+        # Plotting
+        plt.figure(figsize=(9, 6))
+        for ticker, data in tickers:
+            plt.plot(data.index, data['Close'], label=ticker)
+        plt.title(title)
+        plt.xlabel('Date')
+        plt.ylabel('Price in ($ USD)')
+        plt.legend()
+        plt.show()
