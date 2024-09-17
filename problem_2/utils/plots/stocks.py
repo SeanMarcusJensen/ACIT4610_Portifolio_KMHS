f8689d0f18aca16e0e59fc121ce618d2f2998c4c
Sean Marcus Jensen <itskolesean@gmail.com>
2024-09-17 16:31:08 +0200

    Plot

--- problem_2/utils/plots/stocks.py
+++ problem_2/utils/plots/stocks.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 class StockPlotter:
 
     @staticmethod
-    def stock_history(dataframe: pd.DataFrame, title: str) -> None:
+    def stock_history(dataframe: pd.DataFrame, title: str, column: str) -> None:
         # Ensure 'Date' column is in datetime format
         df = dataframe.reset_index()
         df['Date'] = pd.to_datetime(df['Date'], utc=True)
@@ -16,7 +16,7 @@ class StockPlotter:
         # Plotting
         plt.figure(figsize=(9, 6))
         for ticker, data in tickers:
-            plt.plot(data.index, data['Close'], label=ticker)
+            plt.plot(data.index, data[column], label=ticker)
         plt.title(title)
         plt.xlabel('Date')
         plt.ylabel('Price in ($ USD)')
