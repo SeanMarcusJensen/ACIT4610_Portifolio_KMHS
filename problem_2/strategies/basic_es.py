ab947d3bce924149ee7024b2f5be5bc56242a5e5
Sean Marcus Jensen <itskolesean@gmail.com>
2024-09-24 22:00:50 +0200

    Sheees

--- problem_2/strategies/basic_es.py
+++ problem_2/strategies/basic_es.py
@@ -1,6 +1,6 @@
 
 from abc import ABC
-from typing import Any
+from typing import Any, List
 import pandas as pd
 import numpy as np
 
@@ -54,17 +54,6 @@ class RealRepresentation:
             mutation_rate (float): _description_
         """
         
-        """ MUTATION RULE!
-        This rule resets  after every k iterations by
-         =  / c if ps > 1/5
-         =  • c if ps < 1/5
-         =  if ps = 1/5
-        where ps is the % of successful mutations, 0.817  c  1
-
-        Returns:
-            _type_: _description_
-        """
-
         omega_prime = self.omegas * np.exp(
             learning_rate
             * np.array([np.random.normal(0, 1)
@@ -74,12 +63,14 @@ class RealRepresentation:
         x_prime = self.objectives + omega_prime * np.random.normal(0, 1)
         self.omegas = omega_prime
         self.objectives = x_prime
-
+    
+    def get_chromosone(self) -> np.ndarray:
+        return self.objectives
 
 
 class Individual:
     def __init__(self, chromosone_size: int) -> None:
-        self.chromosone: np.ndarray = self.__create_chromosone(chromosone_size)
+        self.chromosone = RealRepresentation(chromosone_size, False)
 
     def mutate(self, mutation_rate: float) -> None:
         """Mutation for ES is done by changing value by adding random noice
@@ -96,43 +87,30 @@ class Individual:
         Args:
             mutation_rate (float): _description_
         """
+
+        """ MUTATION RULE!
+        This rule resets  after every k iterations by
+         =  / c if ps > 1/5
+         =  • c if ps < 1/5
+         =  if ps = 1/5
+        where ps is the % of successful mutations, 0.817  c  1
+
+        Returns:
+            _type_: _description_
+        """
+
         if np.random.rand() <= mutation_rate:
             # Was not its time to mutate :D
             return
 
         # do mutation
-
-        chromosone_number = np.random.randint(0, len(self.chromosone) - 1)
-        self.chromosone[chromosone_number] = np.random.rand()
+        self.chromosone.mutate(0.1) # Learning Rate 0.1
 
     def recombinate(self) -> None:
         pass
 
     def fitness(self) -> None:
         pass
-
-    def __create_chromosone(self, size: int) -> np.ndarray:
-        """Chromosone should consist of three parts:
-        Object Variables, x_1, ..., x_n.
-        Strategy Parameters, such as:
-            Mutation Step Sizes. Omega_1, ..., Omega_n
-                - n_omega is usually either 1 or n
-                - In the most general case their number n_omega =
-                    (n - n_omega/2)(n_omega - 1).
-            Rotation Angles:
-                - k = n(n-1)/2
-
-        Args:
-            size (int): _description_
-
-        Returns:
-            np.ndarray: _description_
-        """
-
-        # Creates a chromosone of [0, 1) with sum of 1. (100%)
-        chromosone = np.random.rand(size)
-        chromosone /= chromosone.sum()
-        return np.array(chromosone)
 
 
 class BasicES:
@@ -155,9 +133,9 @@ class BasicES:
         self.mutation_rate = mutation_rate
         self.generations = generations
     
-    def __create_population(self) -> np.ndarray:
+    def __create_population(self) -> List[Individual]:
         chromosone_size = self.cov.shape[0] # Get number of stocks.
-        population = np.array([Individual(chromosone_size)
+        population = list([Individual(chromosone_size)
                       for _ in range(self.population_size)])
         return population
 
@@ -171,16 +149,16 @@ class BasicES:
         # Create Initial Population
         population = self.__create_population()
 
-        # Evaluate
-
         # Run for all generations
         for gen in range(self.generations):
-            # Evaluate
-
             print(f"Generation: {gen}.")
+
+            # Evaluate
 
             # Select
 
             # Mutate and or Crossover
+            for individual in population:
+                individual.mutate(mutation_rate=0.25) 
 
             # Replace Population
