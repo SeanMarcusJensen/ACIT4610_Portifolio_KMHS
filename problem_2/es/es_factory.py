1a0bada75a5acb79b6141ba189381a0b549272c0
Kristoffer Grinna <77102236+Krigri99@users.noreply.github.com>
2024-10-06 19:15:10 +0200

    40-2-finish-basic-and-advanced-ep (#45)
    * Added basic EP and advanced EP to the EvolutionaryFactory class and in main.ipynb.
    
    * Added title and legend to plots
    
    * Fixed _mutate_objectives in BasicMutator class
    
    * test
    
    ---------
    
    Co-authored-by: Krigri99 <krgr016@student.kristiania.no>

--- problem_2/es/es_factory.py
+++ problem_2/es/es_factory.py
@@ -10,9 +10,27 @@ class EvolutionaryFactory:
     def __init__(self, monthly_returns: pd.DataFrame) -> None:
         self.monthly_returns = monthly_returns
         self.__n_assets = len(monthly_returns.columns)# R^n => R^1
+
+    def create_basic_ep(self, learning_rate: float, n_population: int, n_offsprings: int) -> Strategy:
+        recombinator = NoneCombinator()
+        mutator = BasicMutatorFactory(learning_rate=learning_rate)
+        selector = TournamentSelector(n_population=n_population, n_offsprings=n_offsprings)
+        evaluator = MaximumReturnsEvaluator(self.monthly_returns)
+
+        strategy = Strategy(mutator, evaluator, selector, recombinator)
+        return strategy
     
-    def create_basic_es(self, learning_rate: float, n_population: int, n_offsprings: int) -> Strategy:
-        recombinator = UniformCrossover()
+    def create_advanced_ep(self, learning_rate: float, mutation_rate: float, n_population: int, n_offsprings: int) -> Strategy:
+        recombinator = NoneCombinator()
+        mutator = SelfAdaptiveMutatorFactory(learning_rate=learning_rate, steps=1, mutation_rate=mutation_rate)
+        selector = TournamentSelector(n_population=n_population, n_offsprings=n_offsprings)
+        evaluator = MaximumReturnsEvaluator(self.monthly_returns)
+
+        strategy = Strategy(mutator, evaluator, selector, recombinator)
+        return strategy
+    
+    def create_basic_es(self, learning_rate: float, recombination_rate: float, n_population: int, n_offsprings: int) -> Strategy:
+        recombinator = DiscreteRecombinator(recombination_rate=recombination_rate)
         mutator = BasicMutatorFactory(learning_rate=learning_rate)
         selector = TournamentSelector(n_population=n_population, n_offsprings=n_offsprings)
         evaluator = MaximumReturnsEvaluator(self.monthly_returns)
