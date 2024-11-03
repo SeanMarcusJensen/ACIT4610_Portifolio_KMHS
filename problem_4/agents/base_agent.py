767bd9b7774410f921e5f13950c26d6992bfb9ab
Sean Marcus Jensen <itskolesean@gmail.com>
2024-11-03 13:41:01 +0100

    Quotes

--- problem_4/agents/base_agent.py
+++ problem_4/agents/base_agent.py
@@ -328,14 +328,14 @@ if __name__ == "__main__":
     basic = BasicQAgent(EpsilonGreedy(1.0, 0.999, 0.00), 0.1, 0.95)
     metrics = basic.train(10000, 1000)
     metrics.plot('static/metrics/basic.png')
-    basic.record_video(3, "static/movies/basic")
+    basic.record_video(3, 'static/movies/basic')
 
     sarsa = SarsaAgent(EpsilonGreedy(1.0, 0.999, 0.00), 0.1, 0.95)
     metrics = sarsa.train(10000, 1000)
     metrics.plot('static/metrics/sarsa.png')
-    sarsa.record_video(3,"static/movies/sarsa")
+    sarsa.record_video(3, 'static/movies/sarsa')
 
     dql = DeepQAgent(EpsilonGreedy(1.0, 0.998, 0.00), 0.0001, 0.95, 258, 25000)
     metrics = dql.train(2500, 1000)
     metrics.plot('static/metrics/dql.png')
-    dql.record_video(3, "static/movies/dql")
+    dql.record_video(3, 'static/movies/dql')
